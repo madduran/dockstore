69b47b8da1018201283ce08a7e1328acfe18b45f
mduran <mduran@gsa5.broadinstitute.org>
2018-08-24 16:55:23 -0400

    changing script to handle names

--- writeIGV.sh
+++ writeIGV.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-#this should be either hg19 or hg38
 ref=$1
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
 echo "<Session genome=\""$ref"\" hasGeneTrack=\"true\" hasSequenceTrack=\"true\" locus=\"All\" nextAutoscaleGroup=\"4\" path=\"\" version=\"8\">"
 echo -e "\t<Resources>"
 
-#put in all the file paths
-for i in "${@:2}"; do 
-    echo -e "\t\t<Resource path=\""$i"\"/>" 
+NAMES=""
+FILES=""
+
+while [ "$2" != "" ]; do
+	case "$2" in 
+		-n ) shift
+			 NAMES="$NAMES $2"
+			 ;;
+		* )  FILES="$FILES $2";;
+	esac
+	shift
 done
+
+names=($NAMES)
+files=($FILES)
+
+#if names is empty
+if [ -z "$names" ]; then
+    for ((i=0;i<${#files[@]};++i)); do
+    	echo -e "\t\t<Resource path=\""${files[i]}"\"/>" 
+	done    
+else
+    for ((i=0;i<${#files[@]};++i)); do
+    	echo -e "\t\t<Resource path=\""${files[i]}"\" names=\""${names[i]}"\"/>" 
+	done  
+fi
+
 echo -e "\t</Resources>"
 echo "</Session>"
+
